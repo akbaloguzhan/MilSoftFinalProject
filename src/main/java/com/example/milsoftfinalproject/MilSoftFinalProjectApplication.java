58f767e958004568344b22c894ea6f862917a9cb
akbaloguzhan <ogzhn.akbal@gmail.com>
2023-04-07 14:41:19 +0300

    creating repo and project

---
+++ src/main/java/com/example/milsoftfinalproject/MilSoftFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.milsoftfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilSoftFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilSoftFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/milsoftfinalproject/MilSoftFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.milsoftfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilSoftFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
